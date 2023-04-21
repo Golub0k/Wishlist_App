893b49050d6889bf6b009c97a2f0f7caa4f68291
Golub0k <golub202me@gmail.com>
2023-04-21 18:45:51 +0300

    Add bottom navigate menu

--- app/src/main/java/com/example/wishlistapp/HomePage.java
+++ app/src/main/java/com/example/wishlistapp/HomePage.java
@@ -1,14 +1,53 @@
 package com.example.wishlistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.wishlistapp.databinding.ActivityHomePageBinding;
+import com.example.wishlistapp.databinding.ActivityMainBinding;
+
 public class HomePage extends AppCompatActivity {
+
+    ActivityHomePageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MyWishlistsFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.menu_home:
+                    replaceFragment(new MyWishlistsFragment());
+                    break;
+                case R.id.menu_friends:
+                    replaceFragment(new MyFriendsFragment());
+                    break;
+                case R.id.menu_add:
+                    replaceFragment(new AddWishlistFragment());
+                    break;
+                case R.id.menu_profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
